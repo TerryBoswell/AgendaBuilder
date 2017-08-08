ca2a823b2d3b138fbab49465c810484f8d1fb4b4
TerryBoswell <trboswell74@comcast.net>
2017-08-07 21:09:06 -0400

    ZEN-1119 - WIP

--- app/AgendaBuilderObservable.js
+++ app/AgendaBuilderObservable.js
@@ -3635,6 +3635,36 @@ Ext.define('AgendaBuilderObservable', {
             return out;
         }
 
+        //handling missing support for Safari
+        if (Ext.isSafari && !document.elementsFromPoint)
+        {
+            document.elementsFromPoint = function(x,y) {
+                var elements = [], previousPointerEvents = [], current, i, d;
+
+                    // get all elements via elementFromPoint, and remove them from hit-testing in order
+                while ((current = document.elementFromPoint(x,y)) && elements.indexOf(current)===-1 && current != null) {
+                    
+                        // push the element and its current style
+                    elements.push(current);
+                    previousPointerEvents.push({
+                            value: current.style.getPropertyValue('pointer-events'),
+                            priority: current.style.getPropertyPriority('pointer-events')
+                        });
+                    
+                        // add "pointer-events: none", to get to the underlying element
+                    current.style.setProperty('pointer-events', 'none', 'important'); 
+                }
+
+                    // restore the previous pointer-events values
+                for(i = previousPointerEvents.length; d=previousPointerEvents[--i]; ) {
+                    elements[i].style.setProperty('pointer-events', d.value?d.value:'', d.priority); 
+                }
+                
+                    // return our results
+                return elements;
+            }
+        }
+
         String.prototype.adjustMidnight = function()
         {
             var str = this;
